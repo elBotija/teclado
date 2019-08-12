bf3f286de5ebd9fa6ff067e6391cac7991322d8e
Mauro Martinez <mmartinez@edrans.com>
2019-08-12 13:09:02 -0300

    Updates

---
+++ precache-manifest.e7b0d3a8b0c7d0bb6da06ff337c762cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd12757a50dfe1c476945cde84f4e65",
+    "url": "/teclado/index.html"
+  },
+  {
+    "revision": "0401f306749a5b00671b",
+    "url": "/teclado/static/css/main.39fec4a7.chunk.css"
+  },
+  {
+    "revision": "abd875a7748d65d8cbb8",
+    "url": "/teclado/static/js/2.c97f6f05.chunk.js"
+  },
+  {
+    "revision": "0401f306749a5b00671b",
+    "url": "/teclado/static/js/main.b8ea0ccd.chunk.js"
+  },
+  {
+    "revision": "db7f0d457dd7e699b4e5",
+    "url": "/teclado/static/js/runtime~main.238d6a3c.js"
+  }
+]);
